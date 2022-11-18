58654765c99251936b5782b52eb4cad23d587858
renanmoritz <renan.moritz@gmail.com>
2022-11-17 21:59:30 -0300

    Inclusão de nova classe e seus testes (temperatura)

---
+++ PrimeService/Temperature.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeService
+{
+    public class Temperature
+    {
+        public static class ConversorTemperatura
+        {
+            public static double FahrenheitParaCelsius(double temperatura)
+                //=> (temperatura - 32) / 1.8; // Simulação de falha
+                => Math.Round((temperatura - 32) / 1.8, 2);
+        }
+    }
+}
